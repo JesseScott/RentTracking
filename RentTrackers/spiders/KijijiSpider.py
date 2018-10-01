88b9c32e9c114438b3076aa50a5e5cce77556687
Jesse Scott <jessecolinscott@gmail.com>
2018-09-30 18:24:03 -0700

    Addressing lint warnings on Kijiji spider

--- RentTrackers/spiders/KijijiSpider.py
+++ RentTrackers/spiders/KijijiSpider.py
@@ -13,7 +13,9 @@ KijijiSpider:
                     (2) Determine if kijiji html classes with strigns of numbers
                         are static features or regularly changing.
 """
+
 pageToCrawl = 3
+
 
 class KijijiSpider(scrapy.Spider):
     name = "kijiji"
@@ -22,8 +24,8 @@ class KijijiSpider(scrapy.Spider):
     def start_requests(self):
         base = "https://www.kijiji.ca/b-room-rental-roommate/vancouver/c36l1700287"
         other = "https://www.kijiji.ca/b-room-rental-roommate/vancouver/page-{}/c36l1700287"
-        urls = [other.format(str(i)) for i in range(1,pageToCrawl)]
-        urls.insert(0,base)
+        urls = [other.format(str(i)) for i in range(1, pageToCrawl)]
+        urls.insert(0, base)
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -40,9 +42,9 @@ class KijijiSpider(scrapy.Spider):
         location = response.css("span.address-2932131783::text").extract_first()
         description = remove_tags(response.css("div.descriptionContainer-2901313666").extract_first())
         attributes = [{attr.css("dt::text").extract_first(),attr.css("dd::text").extract_first()} for attr in response.css("dl.itemAttribute-2841032265")]
-        uploadTime = response.css("div.datePosted-1350605722")[0].css("span::attr(title)").extract_first()
-        if uploadTime == None:
-            uploadTime = response.css("div.datePosted-1350605722")[0].css("time::attr(title)").extract_first()
+        upload_time = response.css("div.datePosted-1350605722")[0].css("span::attr(title)").extract_first()
+        if upload_time is None:
+            upload_time = response.css("div.datePosted-1350605722")[0].css("time::attr(title)").extract_first()
         yield {
             "title": title,
             "url": response.url,
@@ -50,5 +52,5 @@ class KijijiSpider(scrapy.Spider):
             "location": location,
             "description": description,
             "attributes": attributes,
-            "datetime": uploadTime
+            "datetime": upload_time
         }
